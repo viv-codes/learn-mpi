d054876dc53a34ad912bdab0cbb455246ab5027d
viv-codes <vhafener@outlook.com>
2022-03-29 14:07:23 -0400

    More exercises!

--- pypractice/ptpcomm/nonblock.py
+++ pypractice/ptpcomm/nonblock.py
@@ -3,6 +3,8 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
+# Nonblocking communications allow for computation and communications at the same time, which can make operations more efficient
+
 if rank == 0:
     data = {'a': 7, 'b': 3.14}
     req = comm.isend(data, dest=1, tag=11)
